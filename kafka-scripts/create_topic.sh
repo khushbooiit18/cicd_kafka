e99ad3a8e36d9a0108dba8706349d9c334059030
khushbooiit18 <khushbooiit18@gmail.com>
2025-04-23 21:32:08 +0530

    Update create_topic.sh

--- kafka-scripts/create_topic.sh
+++ kafka-scripts/create_topic.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-docker exec kafka \
-  kafka-topics --create --topic test-topic --bootstrap-server kafka:9092 --replication-factor 1 --partitions 1 || true
+docker exec kafka kafka-topics --create --topic test-topic --bootstrap-server kafka:9092 --replication-factor 1 --partitions 1 || true
 
