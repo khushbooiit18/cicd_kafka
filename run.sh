7d4664aa354816c01009dbf62107ee03471debfa
khushbooiit18 <khushbooiit18@gmail.com>
2025-04-23 21:27:10 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ docker start zookeeper || docker run -d --name zookeeper --network kafka-net -e 
 docker start kafka || docker run -d --name kafka --network kafka-net -e KAFKA_BROKER_ID=1 -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://kafka:9092 -e KAFKA_LISTENERS=PLAINTEXT://0.0.0.0:9092 -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 -p 9092:9092 confluentinc/cp-kafka:7.2.1
 
 echo "Waiting for Kafka to be ready..."
-sleep 60
+sleep 20
 
 
 # Install Python dependencies
@@ -19,6 +19,6 @@ bash kafka-scripts/create_topic.sh
 
 # Produce and consume
 python3 kafka-scripts/producer.py
-timeout 10 
+timeout 5
 python3 kafka-scripts/consumer.py
 
