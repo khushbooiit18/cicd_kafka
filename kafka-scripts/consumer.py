cb17501f9823f91769929b95d1dbb483b733460e
khushbooiit18 <khushbooiit18@gmail.com>
2025-04-23 23:35:03 +0530

    Update consumer.py

--- kafka-scripts/consumer.py
+++ kafka-scripts/consumer.py
@@ -3,7 +3,7 @@ from kafka import KafkaConsumer
 consumer = KafkaConsumer(
     'test-topic',
     bootstrap_servers='localhost:9092',
-    auto_offset_reset='earliest',
+    auto_offset_reset='from-beginning',
     group_id='test-group'
 )
 
